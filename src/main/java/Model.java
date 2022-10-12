c90093a855f0ea9d913d3e41abfdd196dcfcd700
Danish-mohammed <matifs.1992@gmail.com>
2022-10-12 17:20:32 +0530

    Added:Pacman movement

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -96,13 +96,13 @@ public class Model extends JPanel implements ActionListener {
 
         if (dying) {
 
-//            death();
+            death();
 
         } else {
 
             movePacman();
             drawPacman(g2d);
-//            moveGhosts(g2d);
+            moveGhosts(g2d);
             checkMaze();
         }
     }
@@ -163,80 +163,80 @@ public class Model extends JPanel implements ActionListener {
             inGame = false;
         }
 
-//        continueLevel();
+        continueLevel();
     }
 
-//    private void moveGhosts(Graphics2D g2d) {
-//
-//        int pos;
-//        int count;
-//
-//        for (int i = 0; i < N_GHOSTS; i++) {
-//            if (ghost_x[i] % BLOCK_SIZE == 0 && ghost_y[i] % BLOCK_SIZE == 0) {
-//                pos = ghost_x[i] / BLOCK_SIZE + N_BLOCKS * (int) (ghost_y[i] / BLOCK_SIZE);
-//
-//                count = 0;
-//
-//                if ((screenData[pos] & 1) == 0 && ghost_dx[i] != 1) {
-//                    dx[count] = -1;
-//                    dy[count] = 0;
-//                    count++;
-//                }
-//
-//                if ((screenData[pos] & 2) == 0 && ghost_dy[i] != 1) {
-//                    dx[count] = 0;
-//                    dy[count] = -1;
-//                    count++;
-//                }
-//
-//                if ((screenData[pos] & 4) == 0 && ghost_dx[i] != -1) {
-//                    dx[count] = 1;
-//                    dy[count] = 0;
-//                    count++;
-//                }
-//
-//                if ((screenData[pos] & 8) == 0 && ghost_dy[i] != -1) {
-//                    dx[count] = 0;
-//                    dy[count] = 1;
-//                    count++;
-//                }
-//
-//                if (count == 0) {
-//
-//                    if ((screenData[pos] & 15) == 15) {
-//                        ghost_dx[i] = 0;
-//                        ghost_dy[i] = 0;
-//                    } else {
-//                        ghost_dx[i] = -ghost_dx[i];
-//                        ghost_dy[i] = -ghost_dy[i];
-//                    }
-//
-//                } else {
-//
-//                    count = (int) (Math.random() * count);
-//
-//                    if (count > 3) {
-//                        count = 3;
-//                    }
-//
-//                    ghost_dx[i] = dx[count];
-//                    ghost_dy[i] = dy[count];
-//                }
-//
-//            }
-//
-//            ghost_x[i] = ghost_x[i] + (ghost_dx[i] * ghostSpeed[i]);
-//            ghost_y[i] = ghost_y[i] + (ghost_dy[i] * ghostSpeed[i]);
-//            drawGhost(g2d, ghost_x[i] + 1, ghost_y[i] + 1);
-//
-//            if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
-//                    && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
-//                    && inGame) {
-//
-//                dying = true;
-//            }
-//        }
-//    }
+    private void moveGhosts(Graphics2D g2d) {
+
+        int pos;
+        int count;
+
+        for (int i = 0; i < N_GHOSTS; i++) {
+            if (ghost_x[i] % BLOCK_SIZE == 0 && ghost_y[i] % BLOCK_SIZE == 0) {
+                pos = ghost_x[i] / BLOCK_SIZE + N_BLOCKS * (int) (ghost_y[i] / BLOCK_SIZE);
+
+                count = 0;
+
+                if ((screenData[pos] & 1) == 0 && ghost_dx[i] != 1) {
+                    dx[count] = -1;
+                    dy[count] = 0;
+                    count++;
+                }
+
+                if ((screenData[pos] & 2) == 0 && ghost_dy[i] != 1) {
+                    dx[count] = 0;
+                    dy[count] = -1;
+                    count++;
+                }
+
+                if ((screenData[pos] & 4) == 0 && ghost_dx[i] != -1) {
+                    dx[count] = 1;
+                    dy[count] = 0;
+                    count++;
+                }
+
+                if ((screenData[pos] & 8) == 0 && ghost_dy[i] != -1) {
+                    dx[count] = 0;
+                    dy[count] = 1;
+                    count++;
+                }
+
+                if (count == 0) {
+
+                    if ((screenData[pos] & 15) == 15) {
+                        ghost_dx[i] = 0;
+                        ghost_dy[i] = 0;
+                    } else {
+                        ghost_dx[i] = -ghost_dx[i];
+                        ghost_dy[i] = -ghost_dy[i];
+                    }
+
+                } else {
+
+                    count = (int) (Math.random() * count);
+
+                    if (count > 3) {
+                        count = 3;
+                    }
+
+                    ghost_dx[i] = dx[count];
+                    ghost_dy[i] = dy[count];
+                }
+
+            }
+
+            ghost_x[i] = ghost_x[i] + (ghost_dx[i] * ghostSpeed[i]);
+            ghost_y[i] = ghost_y[i] + (ghost_dy[i] * ghostSpeed[i]);
+            drawGhost(g2d, ghost_x[i] + 1, ghost_y[i] + 1);
+
+            if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
+                    && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
+                    && inGame) {
+
+                dying = true;
+            }
+        }
+    }
 
     private void drawGhost(Graphics2D g2d, int x, int y) {
         g2d.drawImage(ghost, x, y, this);
@@ -351,39 +351,39 @@ public class Model extends JPanel implements ActionListener {
             screenData[i] = levelData[i];
         }
 
-//        continueLevel();
+        continueLevel();
     }
 
-//    private void continueLevel() {
-//
-//        int dx = 1;
-//        int random;
-//
-//        for (int i = 0; i < N_GHOSTS; i++) {
-//
-//            ghost_y[i] = 4 * BLOCK_SIZE; //start position
-//            ghost_x[i] = 4 * BLOCK_SIZE;
-//            ghost_dy[i] = 0;
-//            ghost_dx[i] = dx;
-//            dx = -dx;
-//            random = (int) (Math.random() * (currentSpeed + 1));
-//
-//            if (random > currentSpeed) {
-//                random = currentSpeed;
-//            }
-//
-//            ghostSpeed[i] = validSpeeds[random];
-//        }
-//
-//        pacman_x = 7 * BLOCK_SIZE;  //start position
-//        pacman_y = 11 * BLOCK_SIZE;
-//        pacmand_x = 0;	//reset direction move
-//        pacmand_y = 0;
-//        req_dx = 0;		// reset direction controls
-//        req_dy = 0;
-//        dying = false;
-//    }
-//
+    private void continueLevel() {
+
+        int dx = 1;
+        int random;
+
+        for (int i = 0; i < N_GHOSTS; i++) {
+
+            ghost_y[i] = 4 * BLOCK_SIZE; //start position
+            ghost_x[i] = 4 * BLOCK_SIZE;
+            ghost_dy[i] = 0;
+            ghost_dx[i] = dx;
+            dx = -dx;
+            random = (int) (Math.random() * (currentSpeed + 1));
+
+            if (random > currentSpeed) {
+                random = currentSpeed;
+            }
+
+            ghostSpeed[i] = validSpeeds[random];
+        }
+
+        pacman_x = 7 * BLOCK_SIZE;  //start position
+        pacman_y = 11 * BLOCK_SIZE;
+        pacmand_x = 0;	//reset direction move
+        pacmand_y = 0;
+        req_dx = 0;		// reset direction controls
+        req_dy = 0;
+        dying = false;
+    }
+
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
